f1bd2fc6723d6d03c38e898d9d636276d0fad1c9
Abdelrahmanghoniem <116071431+Abdelrahmanghoniem@users.noreply.github.com>
2024-09-25 18:50:34 +0300

    finished the product model .ts

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -1,20 +1,19 @@
-import mongoose,{Schema,Document} from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
-
-export interface IProduct extends Document{
-    title:string;
-    price:number;
-    image:string;
-    stock:number;
+export interface IProduct extends Document {
+  title: string;
+  image: string;
+  price: number;
+  stock: number;
 }
 
-const ProductSchema=new Schema<IProduct>({
-    title:{type:String,required:true},
-    price:{type:Number,required:true},
-    image:{type:String,required:true},
-    stock:{type:Number,required:true,default:0}
+const productSchema = new Schema<IProduct>({
+  title: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true, default: 0 },
+});
 
-})
+const productModel = mongoose.model<IProduct>('Product', productSchema);
 
-const ProductModel = mongoose.model<IProduct>('Product',ProductSchema)
-export default ProductModel
+export default productModel;
