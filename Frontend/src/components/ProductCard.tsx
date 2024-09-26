18053f0696cd7c48716d481f6110ee9477447e3b
Abdelrahmanghoniem <116071431+Abdelrahmanghoniem@users.noreply.github.com>
2024-09-26 18:36:38 +0300

    all products in Homepage +i ntegeration with API
    "Updated ProductCard component to accept props and refactored HomePage to fetch data from API and handle errors"

--- Frontend/src/components/ProductCard.tsx
+++ Frontend/src/components/ProductCard.tsx
@@ -5,21 +5,27 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-export default function ProductCard() {
+interface Props{
+    id:string,
+    title:string,
+    image:string,
+    price:string
+}
+
+export default function ProductCard({title,image,price}:Props) {
   return (
     <Card >
       <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
+        sx={{ height: 180 }}
+        image={image}
         title="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {title}
         </Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          {price} EGP
         </Typography>
       </CardContent>
       <CardActions>
--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -4,23 +4,39 @@ import Grid from '@mui/material/Grid2';
 
 import ProductCard from '../components/ProductCard'; // Adjust the path based on your project structure
 import { useEffect, useState } from "react";
+import { Product } from "../types/Product";
+import { Base_URL } from "../constants/BaseUrl";
+import { Box } from "@mui/material";
 
 const HomePage = () => {
 
-    const [products,setProducts]=useState([])
+    const [products,setProducts]=useState<Product[]>([])
+    const [error,setError]=useState(false)
+
     useEffect(()=>{
-        fetch("http://localhost:3001/product").then(async(response)=>{
+        const fetchData=async()=>{
+        try{
+        
+            const response=await fetch(`${Base_URL}/product`)
             const data=await response.json()
             setProducts(data)
-        })
-    })
+        } catch {
+            setError(true);
+        }
+    }
+        fetchData();
+    },[])
+
+    if(error){
+        return <Box>sometihing went wrong  please try again</Box>
+    }
 
   return (
     <Container sx={{ mt: 2 }}>
       <Grid container spacing={2} component="div">
-        {products.map((p)=>(
+        {products.map(({_id,title,image,price})=>(
             <Grid  size={{ xs: 12, sm: 6,md:4 }} spacing={2}  component="div">
-                <ProductCard />
+                <ProductCard id={_id} title={title} image={image} price={price} />
               </Grid>
               
         ))}
@@ -31,3 +47,4 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
