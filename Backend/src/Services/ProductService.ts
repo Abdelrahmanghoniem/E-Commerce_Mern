3e89a64b37a98a073f6f430ddb367dde43887ef4
Abdelrahmanghoniem <116071431+Abdelrahmanghoniem@users.noreply.github.com>
2024-09-26 04:40:30 +0300

    started linking the productService with the ProductCard and the HomePage

--- Backend/src/Services/ProductService.ts
+++ Backend/src/Services/ProductService.ts
@@ -6,8 +6,24 @@ export const getAllProducts=async ()=>{
 
 export const seedInitialProducts=async()=>{
     const products=[
-    {title:"MSI GF63 Thin",image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVBCDJh5jSdZN10pDv4OCG4lbyozMhmcy2fA&s"
-        ,price:24000,stock:10}
+    {
+        title:"MSI GF63 Thin labtop",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVBCDJh5jSdZN10pDv4OCG4lbyozMhmcy2fA&s",
+        price:24000,
+        stock:10
+    },
+    {
+        title:"Asus ROG labtop",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDBoTaLj792Fk-39eZx3hB-vWQlcXl7h95Jw&s",
+        price:27000,
+        stock:8
+    },
+    {
+        title:"Hp labtop",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXPNCVu9X30NQIbx70aMIqq1LIDo38XMzFog&s",
+        price:15000,
+        stock:18
+    },
 
     ];
 
--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,5 +1,33 @@
-const HomePage=()=>{
-    return <div>Home page</div>
-}
+import Container from "@mui/material/Container";
+import Grid from '@mui/material/Grid2';
 
-export default HomePage
+
+import ProductCard from '../components/ProductCard'; // Adjust the path based on your project structure
+import { useEffect, useState } from "react";
+
+const HomePage = () => {
+
+    const [products,setProducts]=useState([])
+    useEffect(()=>{
+        fetch("http://localhost:3001/product").then(async(response)=>{
+            const data=await response.json()
+            setProducts(data)
+        })
+    })
+
+  return (
+    <Container sx={{ mt: 2 }}>
+      <Grid container spacing={2} component="div">
+        {products.map((p)=>(
+            <Grid  size={{ xs: 12, sm: 6,md:4 }} spacing={2}  component="div">
+                <ProductCard />
+              </Grid>
+              
+        ))}
+     
+      </Grid>
+    </Container>
+  );
+};
+
+export default HomePage;
