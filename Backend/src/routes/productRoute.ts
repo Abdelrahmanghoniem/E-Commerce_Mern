8c203aa0e9d70e73e2f48aa823ee0ac2b8459619
Abdelrahmanghoniem <116071431+Abdelrahmanghoniem@users.noreply.github.com>
2024-10-03 19:37:42 +0300

    added try and catch method for the product route

--- Backend/src/routes/productRoute.ts
+++ Backend/src/routes/productRoute.ts
@@ -4,8 +4,12 @@ import { getAllProducts } from "../Services/ProductService";
 const router=express.Router()
 
 router.get('/',async (request,response)=>{
-    const products=await getAllProducts();
-    response.status(200).send(products)
+    try{
+        const products=await getAllProducts();
+        response.status(200).send(products)
+    }catch{
+        response.status(500).send({message:"Error fetching products"})
+    }
 })
 
 export default router;
